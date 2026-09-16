e978a726fd7e3b6eae56391269ea113b223f2e0f
Ahmed <ahmed.qassem.aa7@gmail.com>
2026-09-16 22:57:40 +0800

    Add appointment model and comparable sorting

---
+++ src/models/Appointment.java
@@ -0,0 +1,54 @@
+package models;
+
+public class Appointment implements Comparable<Appointment> {
+
+    private String appointmentId;
+    private String patientId;
+    private String doctorId;
+    private String date;
+    private String time;
+    private String status;
+
+    public Appointment(String appointmentId, String patientId, String doctorId,
+                       String date, String time, String status) {
+        this.appointmentId = appointmentId;
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.date = date;
+        this.time = time;
+        this.status = status;
+    }
+
+    public String getAppointmentId() {
+        return appointmentId;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public String getDoctorId() {
+        return doctorId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public int compareTo(Appointment other) {
+        return (date + time).compareTo(other.date + other.time);
+    }
+}
